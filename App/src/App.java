c086cd81d6fe06921a6853ff60a0f39a3f11a241
Eugene <eugeneftw500@gmail.com>
2023-04-05 14:05:18 +0800

    Added File Reader Class

--- App/src/App.java
+++ App/src/App.java
@@ -1,17 +1,20 @@
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 import Login.Account;
-import Projects.Project.Project;
+import Login.FileReader;
+// import Projects.Project.Project;
 import Users.FYP_Coordinator;
 import Users.Student;
 import Users.Supervisor;
+import Users.UserDetails.User;
 
 public class App {
 
 	private ArrayList<Student> studentList;
 	private ArrayList<Supervisor> facultyList;
-	private ArrayList<Project> projectList;
+	// private ArrayList<Project> projectList;
 	private ArrayList<FYP_Coordinator> coordinator;
 	private ArrayList<Account> accountsList;
 
@@ -44,11 +47,12 @@ public class App {
         System.out.println("Enter Password: ");
         inputPassword = sc.nextLine();
 
-        if (/*inputUserID exists in file*/) {
+        if (inputPassword!="" /*inputUserID exists in file*/) {
             String userID = inputUserID;
-            String password = /* correct password */ ;
+            String password = inputPassword /* correct password */ ;
             Account account = new Account(userID, password);
-            account.authenticate(accountList, inputPassword);
+            // account.authenticate(accountList, inputPassword);
+            return true;
         }
         else {
             System.out.println("Invalid user ID!");
@@ -57,19 +61,57 @@ public class App {
     }
 
     private static void initalizeSystem(){ 
+
+        String pathname = System.getProperty("user.dir").concat("\\App\\data\\");
+
         // scan in student list file 
+        try{
+            ArrayList<User> studentList = FileReader.readExcelFile(pathname.concat("student_list.xlsx"), new Student());
+
+            for (int i =0; i<studentList.size(); i++){
+                System.out.println(studentList.get(i).getName());
+                System.out.println(studentList.get(i).getEmail());
+            }
+        }catch(IOException e){
+            e.printStackTrace();
+        }
         // scan in faculty list file
         // scan in rollover project list file
         // scan in fyp coordinator file
     }
 
-	/**
-	 * 
-	 * @param inputFile
-	 */
-	private ArrayList<Object> loadFile(FileIO inputFile) {
-		// TODO - implement App.loadFile
-		throw new UnsupportedOperationException();
-	}
+	// /**
+	//  * 
+	//  * @param inputFile
+	//  */
+	// private ArrayList<Object> loadFile(Object user) {
+		
+    //     String pathname = "..\\data";
+        
+    //     System.getProperty("user.dir");
+
+    //     ArrayList<Object> resultArray = new ArrayList<>();
+
+    //     try{
+    //         File file = new File(pathname);   //creating a new file instance  
+    //         FileInputStream fis = new FileInputStream(file);   //obtaining bytes from the file  
+    //         XSSFWorkbook wb = new XSSFWorkbook(fis);   // Create Workbook
+    //         XSSFSheet sheet=wb.getSheetAt(0);  // Change to Worksheet
+    //         Iterator<Row> itr = sheet.iterator();  // Check row
+            
+    //         while (itr.hasNext()){  
+    //             Row row = itr.next();  
+    //             Iterator<Cell> cellIterator = row.cellIterator();   //iterating over each column  
+    //             while (cellIterator.hasNext())   
+    //             {  
+    //             Cell cell = cellIterator.next();  
+    //             System.out.println(cell.getRow());
+    //             }
+    //             return resultArray;
+    //         }
+    //     }catch(Exception e){
+
+    //     }
+	// }
 
 }
