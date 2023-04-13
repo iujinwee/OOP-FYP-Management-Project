6a93a9003d19c4a931ea80d37f3bc4a80bb0afb7
Eugene <eugeneftw500@gmail.com>
2023-04-14 01:21:12 +0800

    Added GenerateReport Class

---
+++ App/src/Controller/Project/GenerateProjectReportController/SupervisorReport.java
@@ -0,0 +1,21 @@
+package Controller.Project.GenerateProjectReportController;
+
+import Entity.UserClass.UserDetails.User;
+
+public class SupervisorReport extends GenerateReportController {
+    public SupervisorReport(){
+        super();
+    }
+
+    @Override
+    public void header() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'header'");
+    }
+
+    @Override
+    public void body(User user) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'body'");
+    }
+}
--- App/src/Entity/UserClass/FYP_Coordinator.java
+++ App/src/Entity/UserClass/FYP_Coordinator.java
@@ -1,13 +1,15 @@
-package Users;
+package Entity.UserClass;
 
 import Exceptions.InvalidInputException;
-import Projects.ViewProjectsPackage.ViewAvailableProjects;
-import Projects.ViewProjectsPackage.ViewPersonalProjects;
-import Requests.ViewRequestsPackage.ViewAllRequestsHistory;
-import Requests.ViewRequestsPackage.ViewIncomingRequestsHistory;
-import Requests.ViewRequestsPackage.ViewOutgoingRequestsHistory;
-import Requests.ViewRequestsPackage.ViewPendingRequests;
-import Users.UserDetails.UserType;
+
+import java.util.InputMismatchException;
+
+import Controller.Project.GenerateProjectReportController.GenerateReport;
+import Controller.Project.ViewProjectController.*;
+import Controller.Request.ManageRequestController.ManageRequest;
+import Controller.Request.ViewRequestController.ControllerObject.ViewAllRequestsHistory;
+import Controller.Request.ViewRequestController.ControllerObject.ViewPendingRequests;
+import Entity.UserClass.UserDetails.*;
 
 public class FYP_Coordinator extends Supervisor {
 
@@ -26,11 +28,12 @@ public class FYP_Coordinator extends Supervisor {
 	}
 
 	public void viewUserMenu() {
-		System.out.println("=============  MENU  ==============");
+		System.out.println("\n=============  MENU  ==============");
 		System.out.println("[1] View All Requests.");
 		System.out.println("[2] Manage Requests.");
 		System.out.println("[3] View Pending Requests.");
 		System.out.println("[4] View Projects.");
+		System.out.println("[5] Generate Project Report.");
 		System.out.println("[0] Exit Program.");
 	}
 	
@@ -38,46 +41,44 @@ public class FYP_Coordinator extends Supervisor {
 	public void getInput() throws InvalidInputException{
 		int choice = -1;
 
-		loadFiles(reload);
-		reload = false;
-
 		while (choice != 0){	
 			
 			// Show User Menu
 			viewUserMenu();
 
 			// Get Input 
-			System.out.println("\nEnter your option: ");
-			choice = super.sc.nextInt();
+			System.out.printf("\nEnter Option: ");
+			choice = sc.nextInt();
 
 			switch(choice){
 				case 1: 
-					System.out.println("Option [1] selected! - View All Requests");
+					System.out.println("Option [1] selected! - View All Requests\n");
 					new ViewAllRequestsHistory(this);
 					break;
 
 				case 2: 
-					System.out.println("Option [2] selected! - Manage Requests.");
-					new ViewPendingRequests(this);
-					manageRequests();
+					System.out.println("Option [2] selected! - Manage Requests.\n");
+					new ManageRequest(this);
+
 					break;
 
 				case 3:
-					System.out.println("Option [3] selected! - View Pending Requests.");
+					System.out.println("Option [3] selected! - View Pending Requests.\n");
 					new ViewPendingRequests(this);
 					break;
 
 				case 4:	
-					System.out.println("Option [4] selected! - View Projects.");
+					System.out.println("Option [4] selected! - View Projects.\n");
 					viewProjectOption();
 					break;
 
 				case 5: 
-					System.out.println("Option [5] selected! - Generate Project Report."); 
-				
+					System.out.println("Option [5] selected! - Generate Project Report.\n"); 
+					new GenerateReport();
+					break;
 			
 				case 0: 
-					System.out.println("Option [0] selected! - Exit Program");
+					System.out.println("Option [0] selected! - Exit Program\n");
 					break;
 
 				default:
@@ -90,7 +91,8 @@ public class FYP_Coordinator extends Supervisor {
 		System.out.println("[1] View Personal Projects");
 		System.out.println("[2] View All Projects");
 		System.out.println("[0] Exit");
-
+		System.out.printf("\nEnter Option: ");
+		
 		switch(sc.nextInt()){
 			case 1:
 				new ViewPersonalProjects(this);
@@ -102,24 +104,4 @@ public class FYP_Coordinator extends Supervisor {
 				break;
 		}
 	}
-
-	// @Override
-	// public void manageRequests() {
-
-	// 	//manage requests
-	// 	System.out.println("Enter RequestID to Approve/Reject: ");
-	// 	int reqID = sc.nextInt();
-
-	// 	Request currentReq = reqDB.findInstance(reqID);
-
-	// 	System.out.println("Approve/ Reject");
-	// 	System.out.println("[1] Approve");
-	// 	System.out.println("[0] Reject");
-	// 	int choice = sc.nextInt();
-
-	// 	// currentReq.enactRequest(choice);
-
-	// 	reqDB.exportDB();
-
-	// }
 }
