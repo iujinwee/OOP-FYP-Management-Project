e543dccfd084a31a4983fea7abf2b2d243dc893b
Liek Zhe <93731190+liekzhe@users.noreply.github.com>
2023-04-16 01:09:34 +0800

    testing

--- App/src/Controller/Project/ModifyProjectController/ControllerObject/ChangeProjectSupervisor.java
+++ App/src/Controller/Project/ModifyProjectController/ControllerObject/ChangeProjectSupervisor.java
@@ -2,13 +2,17 @@ package Controller.Project.ModifyProjectController.ControllerObject;
 
 import Controller.Project.ModifyProjectController.ModifyProjectController;
 import Entity.DatabaseClass.SupervisorDB;
+import Entity.DatabaseClass.ProjectDB;
 import Entity.UserClass.Supervisor;
+import Entity.ProjectClass.Project;
+import Entity.ProjectClass.ProjectStatus;
 
 public class ChangeProjectSupervisor extends ModifyProjectController {
 
     private int projID;
     private Supervisor supervisor;
     public SupervisorDB supDB;
+    public ProjectDB projDB;
     
     public ChangeProjectSupervisor(int projID, String supID){
         super();
@@ -23,11 +27,29 @@ public class ChangeProjectSupervisor extends ModifyProjectController {
     
     @Override
 	public void updateDB() {		
+        projDB = new ProjectDB();
         supDB = new SupervisorDB();
         supDB.findInstance(projDB.findInstance(projID).getSupervisor().getUserID()).removeAssignedProjects();
+        // if(supDB.findInstance(projDB.findInstance(projID).getSupervisor().getUserID()).getNumAssignedProjects() < 2){
+        //     for (Object obj : projDB.objectDB) {
+        //         Project curProject = (Project) obj;
+        //         if (supDB.findInstance(supervisor.getUserID()).getUserID().compareTo(curProject.getSupervisorID())==0 && curProject.getProjectStatus() == ProjectStatus.UNAVAILABLE) {
+        //             curProject.setProjectStatus(ProjectStatus.AVAILABLE); // TO CHANGE
+        //         }
+        //     }
+        // }
         supDB.findInstance(supervisor.getUserID()).addAssignedProjects();
-        supDB.exportDB();
         projDB.findInstance(projID).setSupervisor(supervisor);
+        // if(supervisor.getNumAssignedProjects() == 2){
+        //     for (Object obj : projDB.objectDB) {
+        //         Project curProject = (Project) obj;
+        //         if (supDB.findInstance(supervisor.getUserID()).getUserID().compareTo(curProject.getSupervisorID())==0 && curProject.getProjectStatus() == ProjectStatus.AVAILABLE) {
+        //             curProject.setProjectStatus(ProjectStatus.UNAVAILABLE); // TO CHANGE
+        //         }
+        //     }
+        // }
+        supDB.exportDB();
+        
 
         System.out.printf("Successfully changed supervisor to %s\n", supervisor.getName());
 	}
