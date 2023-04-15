d46bab56c3d56f4d8e733dfa5ede944c6bf24b17
shermaineng <sng112@e.ntu.edu.sg>
2023-04-15 23:58:20 +0800

    javadoc commit 2

--- App/src/Boundaries/Menu/GetInputInterface.java
+++ App/src/Boundaries/Menu/GetInputInterface.java
@@ -4,7 +4,7 @@ import Exceptions.InvalidInputException;
 
 public interface GetInputInterface {
     /**
-	 * Abstract Function to get user input. Throws InvalidInputException when invalid inputs received.
+	 * Abstract method to get user input. Throws InvalidInputException when invalid inputs received.
 	 * @throws InvalidInputException 
 	 * @Invalid Beyond the Option Selection given or Non-numerical input 
 	 */
