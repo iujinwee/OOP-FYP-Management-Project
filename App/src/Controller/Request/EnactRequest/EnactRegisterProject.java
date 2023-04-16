ba59ab6fdb2d7b2022da89d2f8b3e8d0643c9763
Liek Zhe <93731190+liekzhe@users.noreply.github.com>
2023-04-16 19:59:40 +0800

    edits for reg proj

--- App/src/Controller/Request/EnactRequest/EnactRegisterProject.java
+++ App/src/Controller/Request/EnactRequest/EnactRegisterProject.java
@@ -2,23 +2,48 @@ package Controller.Request.EnactRequest;
 
 import Controller.Project.ModifyProject.RegisterProject;
 import Entity.UserClass.Student;
+import Entity.UserClass.Supervisor;
 import Entity.DatabaseClass.ProjectDB;
 import Entity.DatabaseClass.StudentDB;
+import Entity.DatabaseClass.SupervisorDB;
 import Entity.ProjectClass.ProjectStatus;
+import Entity.RequestClass.Request;
+import Entity.RequestClass.RequestStatus;
+import Entity.RequestClass.RequestType;
+import Entity.ProjectClass.*;
 
 public class EnactRegisterProject extends EnactRequest {
 
-    public EnactRegisterProject(int reqID){
+    public EnactRegisterProject(int reqID) {
         super(reqID);
     }
 
     @Override
     public void approve() {
         StudentDB stuDB = new StudentDB();
-        
+
         new RegisterProject(request.getProjectID(), (Student) request.getFromUser());
         stuDB.findInstance(request.getfromUserID()).setAssigned(true);
         stuDB.exportDB();
+
+        SupervisorDB supDB = new SupervisorDB();
+        ProjectDB projDB = new ProjectDB();
+        Project curProject = projDB.findInstance(request.getProjectID());
+        Supervisor curSup = supDB.findInstance(curProject.getSupervisorID());
+
+        if (curSup.getNumAssignedProjects() >= 2) {
+            for (Object obj : reqDB.objectDB) {
+                Request curReq = (Request) obj;
+                boolean isReg = curReq.getRequestType() == RequestType.REGISTERPROJECT;
+                boolean own = (projDB.findInstance(curReq.getProjectID())).getSupervisorID()
+                        .compareTo(curSup.getUserID()) == 0;
+                boolean pending = curReq.getRequestStatus() == RequestStatus.PENDING;
+                boolean notsame = curReq.getfromUserID().compareTo(request.getfromUserID()) != 0;
+                if (isReg && own && pending && notsame) {
+                    curReq.setRequestStatus(RequestStatus.REJECTED);
+                }
+            }
+        }
     }
 
     @Override
