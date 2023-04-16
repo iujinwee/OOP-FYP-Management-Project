bd8ecbc3b3d875fb628b4a7807507f6fc023d368
Liek Zhe <93731190+liekzhe@users.noreply.github.com>
2023-04-16 21:17:31 +0800

    create proj title not empty

--- App/src/Controller/Project/GetInputModifyProject/CreateProject.java
+++ App/src/Controller/Project/GetInputModifyProject/CreateProject.java
@@ -4,70 +4,74 @@ import Entity.ProjectClass.Project;
 import Entity.UserClass.Supervisor;
 import Exceptions.InvalidInputException;
 
-public class CreateProject extends GetInputModifyProject{
+public class CreateProject extends GetInputModifyProject {
 
 	private Supervisor supervisor;
 	private Project newProject;
 	private String newTitle;
 
-	/** 
-     * Create Project Constructor.
-     * @param supervisor Supervisor Object
-     */
-    public CreateProject(Supervisor supervisor) {
+	/**
+	 * Create Project Constructor.
+	 * 
+	 * @param supervisor Supervisor Object
+	 */
+	public CreateProject(Supervisor supervisor) {
 		super();
-        this.supervisor = supervisor;
+		this.supervisor = supervisor;
 		handleException();
-    }
+	}
 
-	
 	@Override
 	public void getInput() throws InvalidInputException {
-        //ask rest of project details here
-		//if num of assigned > 2 then cannot create project alr 
-		//else num of assigned +1
-		
+		// ask rest of project details here
+		// if num of assigned > 2 then cannot create project alr
+		// else num of assigned +1
+
 		if (supervisor.getNumAssignedProjects() < 2) {
 			// Get Project Title
 			System.out.printf("\nInput your new project title: ");
 			newTitle = sc.nextLine();
-			
-			System.out.printf("Creating New Project: [%s] Confirm?\n", newTitle);
-			System.out.println("[1] Yes");
-			System.out.println("[0] No");
-			System.out.printf("Enter option: ");
-			int choice = sc.nextInt();
 
-			if(choice==1){
-				updateDB();
-				System.out.printf("\nSuccessfully created Project - %s\n", newTitle);
-
-			}else if(choice == 0){
-				System.out.println("Creating New Project operation is cancelled.\n");
-				
-			}else{
-				throw new InvalidInputException(choice);
+			if (newTitle.isBlank()) {
+				System.out.println("\nTitle cannot be empty!\n");
 			}
-		
-		}
-		else {
+
+			else {
+				System.out.printf("Creating New Project: [%s] Confirm?\n", newTitle);
+				System.out.println("[1] Yes");
+				System.out.println("[0] No");
+				System.out.printf("Enter option: ");
+				int choice = sc.nextInt();
+
+				if (choice == 1) {
+					updateDB();
+					System.out.printf("\nSuccessfully created Project - %s\n", newTitle);
+
+				} else if (choice == 0) {
+					System.out.println("Creating New Project operation is cancelled.\n");
+
+				} else {
+					throw new InvalidInputException(choice);
+				}
+			}
+
+		} else {
 			System.out.println("Error! You are unable to create anymore projects!\n");
 		}
 	}
 
-
-    @Override
-    public void updateDB() {
+	@Override
+	public void updateDB() {
 
 		// Create new project
-		int newID = ++projDB.size; 
-		newProject = new Project(newID); 
-		newProject.setSupervisor(supervisor); 
+		int newID = ++projDB.size;
+		newProject = new Project(newID);
+		newProject.setSupervisor(supervisor);
 		newProject.setProjectTitle(newTitle);
 
 		// Add to database
 		projDB.objectDB.add(newProject);
 		exportDB();
-    }
+	}
 
 }
